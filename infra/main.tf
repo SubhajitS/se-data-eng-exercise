138716a0f4551f18a0f12571206e6e571d695c5f
Subhajit Saha <subhajit.saha@equalexperts.com>
2024-07-31 11:10:06 +0530

    Add bigquery dataset and datatable

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
     }
   }
@@ -11,11 +11,31 @@ provider "google" {
   project = var.project
 }
 
-resource "google_storage_bucket" "se-data-landing" {
-  name = var.storage_bucket.name
-  location = var.storage_bucket.location
-  storage_class = var.storage_bucket.storage_class
-  force_destroy = var.storage_bucket.force_destroy
-  public_access_prevention = var.storage_bucket.public_access_prevention
+resource "google_storage_bucket" "se-data-storage-bucket" {
+  name                        = var.storage_bucket.name
+  location                    = var.location
+  storage_class               = var.storage_bucket.storage_class
+  force_destroy               = var.storage_bucket.force_destroy
+  public_access_prevention    = var.storage_bucket.public_access_prevention
   uniform_bucket_level_access = var.storage_bucket.uniform_bucket_level_access
+}
+
+resource "google_bigquery_dataset" "se-data-dataset" {
+  dataset_id  = var.dataset.name
+  location    = var.location
+  description = var.dataset.description
+  delete_contents_on_destroy = var.dataset.delete_contents_on_destroy
+
+  access {
+    role          = var.dataset.access.role
+    user_by_email = var.dataset.access.email
+  }
+}
+
+resource "google_bigquery_table" "se-data-raw-table" {
+  depends_on = [ google_bigquery_dataset.se-data-dataset ]
+  dataset_id = google_bigquery_dataset.se-data-dataset.dataset_id
+  table_id   = var.datatable.name
+  deletion_protection = var.datatable.deletion_protection
+  schema     = var.datatable.schema
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,208 @@
 variable "project" {
-    type = string
-    default = "ee-india-se-data"
+  type    = string
+  default = "ee-india-se-data"
+}
+variable "location" {
+  type    = string
+  default = "ASIA-SOUTH1"
 }
 variable "storage_bucket" {
-    type = object({
-      name = string
-      location = string
-      storage_class = string
-      public_access_prevention = string
-      uniform_bucket_level_access = bool
-      force_destroy = bool
-    })
-    default = {
-      name = "se-data-landing-subhajit"
-      location = "ASIA-SOUTH1"
-      storage_class = "STANDARD"
-      public_access_prevention = "enforced"
-      force_destroy = true
-      uniform_bucket_level_access = true
+  type = object({
+    name                        = string
+    storage_class               = string
+    public_access_prevention    = string
+    uniform_bucket_level_access = bool
+    force_destroy               = bool
+  })
+  default = {
+    name                        = "se-data-landing-subhajit"
+    storage_class               = "STANDARD"
+    public_access_prevention    = "enforced"
+    force_destroy               = true
+    uniform_bucket_level_access = true
+  }
+}
+
+variable "dataset" {
+  type = object({
+    name                       = string
+    description                = string
+    delete_contents_on_destroy = bool
+    access                     = map(string)
+  })
+  default = {
+    name                       = "movies_data_subhajit"
+    description                = "This is a test dataset"
+    delete_contents_on_destroy = true
+    access = {
+      "role"  = "OWNER"
+      "email" = "subhajit.saha@equalexperts.com"
     }
+  }
+}
+
+variable "datatable" {
+  type = object({
+    name                = string
+    deletion_protection = bool
+    schema              = string
+  })
+  default = {
+    name                = "movies_raw"
+    deletion_protection = false
+    schema              = <<EOF
+  [
+    {
+      "name":"adult",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"belongs_to_collection",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"budget",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"genres",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"homepage",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"id",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"imdb_id",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"original_language",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"original_title",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"overview",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"popularity",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"poster_path",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"production_companies",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"production_countries",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"release_date",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"revenue",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"runtime",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"spoken_languages",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"status",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"tagline",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"title",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"video",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"vote_average",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"vote_count",
+      "type":"STRING",
+      "mode":"NULLABLE",
+      "description":""
+    },
+    {
+      "name":"load_date",
+      "type":"DATETIME",
+      "mode":"NULLABLE",
+      "description":""
+    }
+  ]
+  EOF
+  }
 }
