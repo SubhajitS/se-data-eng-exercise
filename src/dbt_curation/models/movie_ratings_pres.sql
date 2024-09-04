760304d8bbc0c2bc35cd11e05953b99c0f7ee083
Subhajit Saha <subhajit.saha@equalexperts.com>
2024-09-04 16:50:07 +0530

    Refactor using CTE

--- src/dbt_curation/models/movie_ratings_pres.sql
+++ src/dbt_curation/models/movie_ratings_pres.sql
@@ -1,13 +1,7 @@
 {{ config(materialized='table') }}
 
-SELECT 
-    movieId, 
-    title,
-    median,
-    COUNT(1) AS numberOfRatings,
-    DENSE_RANK() OVER (ORDER BY median DESC) AS rank
-FROM
-    (SELECT
+WITH MOVIE_BY_MEDIAN_RATING AS (
+    SELECT
         ratings.movieId, 
         movies.title,
         PERCENTILE_CONT(ratings.rating, 0.5) OVER(PARTITION BY ratings.movieId) AS median
@@ -15,5 +9,13 @@ FROM
         {{ ref("ratings_curate") }} ratings
     INNER JOIN 
         {{ ref("movies_curate") }} movies ON ratings.movieId=movies.id
-    )
+)
+SELECT 
+    movieId, 
+    title,
+    median,
+    COUNT(1) AS numberOfRatings,
+    DENSE_RANK() OVER (ORDER BY median DESC) AS rank
+FROM
+    MOVIE_BY_MEDIAN_RATING
 GROUP BY movieId,title,median
